cd867bb8c30399414a215bbf4c074a36d529b8fb
võ văn tín <vovantin9502@gmail.com>
2023-07-07 09:12:01 +0700

    tin c1

--- src/main/java/com/example/case_study/model/DetailRoom.java
+++ src/main/java/com/example/case_study/model/DetailRoom.java
@@ -4,18 +4,26 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "details_room")
-@IdClass(DetailId.class)
 public class DetailRoom {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "detail_room_id")
+    private Integer id;
     @ManyToOne
     @JoinColumn(name = "id_room",referencedColumnName = "id_room")
     private Room room;
-    @Id
     @ManyToOne
     @JoinColumn(name = "facility_id",referencedColumnName ="facility_id" )
     private Facility facility;
 
     private Integer amount;
+
+    public DetailRoom(Integer id, Room room, Facility facility, Integer amount) {
+        this.id = id;
+        this.room = room;
+        this.facility = facility;
+        this.amount = amount;
+    }
 
     public DetailRoom(Room room, Facility facility, Integer amount) {
         this.room = room;
@@ -55,5 +63,11 @@ public class DetailRoom {
         this.facility = facility;
     }
 
-}
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
