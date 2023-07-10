2b6ea8ab567f1c50fb8733b135eab4b8f5e9ccf6
Admin <vuthanhlong0906438630@gmail.com>
2023-07-10 16:38:34 +0700

    thien c15

--- src/main/java/com/example/case_study/controller/FacilityControler.java
+++ src/main/java/com/example/case_study/controller/FacilityControler.java
@@ -1,13 +1,17 @@
 package com.example.case_study.controller;
 
+import com.example.case_study.model.DetailRoom;
+import com.example.case_study.model.DetailRoomDto;
+import com.example.case_study.model.Facility;
+import com.example.case_study.model.Room;
 import com.example.case_study.service.detail_room.IDetailRoomService;
 import com.example.case_study.service.facility.IFacilityService;
 import com.example.case_study.service.room.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/facility")
@@ -21,6 +25,27 @@ public class FacilityControler {
     @GetMapping("")
     public String displayRoom(Model model){
         model.addAttribute("facilityList",facilityService.display());
+        model.addAttribute("facilityCreate",new Facility());
         return "/facility/list";
+    }
+    @PostMapping("create")
+    public String create(@ModelAttribute Facility facility, RedirectAttributes redirectAttributes) {
+        facilityService.save(facility);
+        redirectAttributes.addFlashAttribute("msg","Create success");
+        return "redirect:/facility";
+    }
+    @PostMapping("update")
+    public String update(@RequestParam("nameFacility") String nameFacility,@RequestParam("id") Integer id,RedirectAttributes redirectAttributes) {
+        Facility facility = facilityService.detailFacility(id);
+        facility.setName(nameFacility);
+        facilityService.edit(facility);
+        redirectAttributes.addFlashAttribute("msg", "Update Success");
+        return "redirect:/facility";
+    }
+    @PostMapping("delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        facilityService.delete(id);
+        redirectAttributes.addFlashAttribute("msg", "Delete success");
+        return "redirect:/facility";
     }
 }
