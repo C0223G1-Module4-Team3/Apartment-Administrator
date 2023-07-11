784b01b104f640920f86efc8ac7ed0d19bbf1bad
cao <vietcao2510@gmail.com>
2023-07-11 09:21:11 +0700

    Cao commit employee cm 15

--- src/main/java/com/example/case_study/repository/IEmployeeRepository.java
+++ src/main/java/com/example/case_study/repository/IEmployeeRepository.java
@@ -9,8 +9,9 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface IEmployeeRepository extends JpaRepository<Employee,Integer> {
+public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
     Page<Employee> findAllByFlagDeleteIsFalseAndAccountUserIsNull(Pageable pageable);
+
     Employee findById(int id);
 
     List<Employee> findAllByFlagDeleteFalseAndAccountUserIsNotNull();
@@ -18,11 +19,12 @@ public interface IEmployeeRepository extends JpaRepository<Employee,Integer> {
     List<Employee> findAllByFlagDeleteFalseAndAccountUserIsNull();
 
     List<Employee> findAllByFlagDeleteFalseAndAccountUserIsNullAndGenderIsTrue();
+
     List<Employee> findAllByFlagDeleteFalseAndAccountUserIsNullAndGenderIsFalse();
 
     Employee findByPhoneNumber(String phone);
-    @Query(value = "select * from employee as c where (c.name like concat('%', :name ,'%') or c.name='') and( c.phone_number like concat('%', :phoneNumber ,'%') or c.phone_number='')and (c.citizen_id like concat('%', :citizenId ,'%') or c.citizen_id='')", nativeQuery = true)
 
+    @Query(value = "select * from employee as c where (c.name like concat('%', :name ,'%') or c.name='') and( c.phone_number like concat('%', :phoneNumber ,'%') or c.phone_number='')and (c.citizen_id like concat('%', :citizenId ,'%') or c.citizen_id='')", nativeQuery = true)
     Page<Employee> findAllByNameOrPhoneNumberOrCitizenId
             (Pageable pageable, @Param(value = "name") String name, @Param(value = "citizenId") String citizenId, @Param(value = "phoneNumber") String phoneNumber);
 }
